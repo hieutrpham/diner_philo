5fc3c991de7e5c2d5c24dd35525c0c981df085cb
hieutrpham <hieutrungpham91@gmail.com>
2025-10-22 17:05:41 +0300

    feat: created 3 philosophers and 3 forks using threads and mutexes. there is deadlock between P1 and P3.

--- philo/main.c
+++ philo/main.c
@@ -1,47 +1,84 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <pthread.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trupham <trupham@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 11:14:13 by trupham           #+#    #+#             */
+/*   Updated: 2025/10/22 12:30:03 by trupham          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define ITERATION 10000000
-#define THREAD_COUNT 16
+#include "philo.h"
 
-pthread_t threads[THREAD_COUNT];
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+#define THREAD_COUNT 3
 
-#if 0
-int count = 0;
-
-void *routine()
+int get_time()
 {
-	pthread_mutex_lock(&mutex);
-	for (int i = 0; i < ITERATION; i++)
-		count++;
-	pthread_mutex_unlock(&mutex);
-	return NULL;
-}
-#else
-
-_Atomic int count = 0;
-void *routine()
-{
-	for (int i = 0; i < ITERATION; i++)
-		count++;
-	return NULL;
+	struct timeval tv;
+	int t = gettimeofday(&tv, NULL);
+	if (t < 0)
+		return (printf("%s\n", strerror(errno)));
+	return tv.tv_sec*1000 + tv.tv_usec/1000;
 }
 
-#endif
+void *routine(void *arg)
+{
+	t_philo *philo = (t_philo *)arg;
 
+	printf("thread %d pick up left fork\n", philo->id);
+	pthread_mutex_lock(philo->lf);
+	printf("thread %d pick up right fork\n", philo->id);
+	pthread_mutex_lock(philo->rf);
+	printf("thread %d is eating\n", philo->id);
+	usleep(500);
+	printf("thread %d release both forks\n", philo->id);
+	pthread_mutex_unlock(philo->lf);
+	pthread_mutex_unlock(philo->rf);
+	printf("thread %d is thinking\n", philo->id);
+	usleep(500);
+	printf("thread %d is sleeping\n", philo->id);
+	usleep(500);
+	return NULL;
+}
+
+//thread is philo
+//mutex is fork
 int main(void)
 {
+	t_philo philos[THREAD_COUNT];
+	pthread_mutex_t forks[THREAD_COUNT];
+
+	// initialize the philos
 	for (int i = 0; i < THREAD_COUNT; i++)
 	{
-		pthread_create(&threads[i], NULL, routine, NULL);
+		philos[i].id = i + 1;
+		if (i == THREAD_COUNT - 1)
+			philos[i].lf = &forks[0];
+		else
+			philos[i].lf = &forks[philos[i].id];
+		if (i == 0)
+		{
+			philos[i].rf = &forks[i];
+			philos[i].lf = &forks[philos[i].id];
+		}
+		else
+			philos[i].rf = &forks[philos[i].id - 1];
+		printf("philo %d lf: %p rf: %p\n", philos[i].id, philos[i].lf, philos[i].rf);
 	}
+
+	// create threads as philos
 	for (int i = 0; i < THREAD_COUNT; i++)
 	{
-		pthread_join(threads[i], NULL);
+		pthread_create(&philos[i].thread, NULL, routine, &philos[i]);
 	}
-	printf("count: %d\n", count);
+	
+	//join all philos
+	for (int i = 0; i < THREAD_COUNT; i++)
+	{
+		pthread_join(philos[i].thread, NULL);
+	}
+
 	return 0;
 }
---
+++ philo/philo.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trupham <trupham@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 11:13:39 by trupham           #+#    #+#             */
+/*   Updated: 2025/10/22 11:24:52 by trupham          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+typedef struct s_philo
+{
+	uint id;
+	pthread_t thread;
+	pthread_mutex_t *lf;
+	pthread_mutex_t *rf;
+} t_philo;
+
+typedef struct s_sim
+{
+	t_philo *philos;
+} t_sim;
